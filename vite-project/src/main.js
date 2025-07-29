ef8def192359b6dd82aad2a413affd4519009bc7
Olaciregui <juanjoseolaciregui85@gmail.com>
2025-07-29 16:09:39 -0500

    commit

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -3,6 +3,8 @@ const apiURL = "http://localhost:3000/tareas"
 
 const form = document.getElementById("formData")
 const list = document.getElementById('tasks')
+const listcomp = document.getElementById('tasksComp')
+const listpend = document.getElementById('tasksPend')
 const input= document.getElementById('date')
 
 
@@ -15,13 +17,33 @@ try{
   console.log(data)
   console.log(completos)
   console.log(pendiente)
-   tasks.innerHTML=""
+   listcomp.innerHTML=""
+   list.innerHTML=""
+   listpend.innerHTML=""
+   input.value =""
    
      
     data.forEach((d)=>{
       const li = document.createElement("li")
-      li.innerHTML = `<input type="checkbox" id="check" ${d.completada? 'checked': ''} onchange="toggleEstado(${d.id}, this.checked)"><span style="text-decoration:${d.completada? 'line-through': 'none'};">${d.titulo}`
+      console.log(d)
+      li.innerHTML = `<button onclick="deleteData(${d.id})" >🗑️</button><input type="checkbox" id="check" ${d.completada? 'checked': ''} onchange="toggleEstado(${d.id}, this.checked)"><span style="text-decoration:${d.completada? 'line-through': 'none'}; ">${d.titulo}</span> `
       tasks.appendChild(li)
+     
+     
+    })
+
+     completos.forEach((c)=>{
+      const li = document.createElement("li")
+      li.innerHTML = `<button onclick="deleteData(${c.id})">🗑️</button><input type="checkbox" id="check" ${c.completada? 'checked': ''} onchange="toggleEstado(${c.id}, this.checked)"><span style="text-decoration:${c.completada? 'line-through': 'none'};">${c.titulo}</span>`
+      listcomp.appendChild(li)
+    
+     
+    })
+        pendiente.forEach((p)=>{
+      const li = document.createElement("li")
+      li.innerHTML = `<button onclick="deleteData(${p.id} )" >🗑️</button><input type="checkbox" id="check" ${p.completada? 'checked': ''} onchange="toggleEstado(${p.id}, this.checked)"><span style="text-decoration:${p.completada? 'line-through': 'none'};">${p.titulo}</span>`
+      listpend.appendChild(li)
+      
      
     })
     
@@ -54,9 +76,9 @@ form.addEventListener("submit",async(e)=>{
       },
       body: JSON.stringify(newObject)
     })
-      
     getData()
-     
+      
+    
   }catch{}
 })
 
@@ -80,6 +102,22 @@ async function toggleEstado(id, estado){
   getData()
 }
 
+
+async function deleteData(id){
+
+  try{
+     const response = await fetch(`${apiURL}/${id}`,{
+    method: 'DELETE',
+      headers:{
+        'Content-Type': 'applications/json'
+      }
+    })
+
+  }catch(error){
+    console.error("qww",error)
+  }
+getData()
+}
 
      
       
